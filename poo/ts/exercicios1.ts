a25b73efacb39295aaf737ac273d486f4e9dac4d
mariaeduanea <153656590+mariaeduanea@users.noreply.github.com>
2024-02-21 17:31:35 -0300

    exercicios

--- poo/ts/exercicios1.ts
+++ poo/ts/exercicios1.ts
@@ -1,18 +1,47 @@
 //cadastro de funcionários
 //detalhes pessoais e proficionais dos colaboradores 
-class Funcionario {
-    nome: string;
-    email: string;
-    telefone: string;
+class Pessoa{
+    nome:string;
+    email:string;
+    telefone:string;
+    data_nascimento:Date;
+    rg:string;
+    endereco:string[];
+    constructor(nome:string,email:string,telefone:string,data_nascimento:Date, rg:string,endereco:string[]){
+        this.nome=nome;
+        this.endereco=endereco;
+        this.email=email;
+        this.telefone=telefone;
+        this.data_nascimento=data_nascimento;
+        this.rg=rg;
+    }
+    mostrarDados(){
+        console.log('dados do funcionario:')
+        console.log(`nome:${this.nome}`)
+        console.log(`telefone ${this.telefone}`)
+        console.log(`E-mail:${this.email}`)  
+        console.log(`Data de nascimento:${this.data_nascimento.toLocaleDateString("pt-br")}`)
+        console.log(`endereço:${this.endereco}`)
+        console.log(`rg:${this.rg}`)
+
+    }
+}
+class Funcionario extends Pessoa{
     cargo: string;
-    constructor(nome: string, email: string, telefone: string, cargo: string) {
+    matricula:string;
+    constructor(matricula:string, endereco:string, nome: string, email: string, telefone: string, data_nascimento:Date, cargo: string, rg:string) {
+       super(nome, endereco, email, data_nascimento, telefone, rg)
         this.cargo = cargo;
-        this.email = email;
-        this.telefone = telefone;
-        this.nome = nome;
+        this.matricula=matricula;
+    }
+    mostrarDados(){
+        super.mostrarDados();
+        console.log(`cargo:${this.cargo}`)
+        console.log(`Matricula:${this.matricula}`)
+
     }
 
-}
+    }
 const funcionario1 = new Funcionario('eduarda', 'mariaeduada@gmail.com', '998338875', 'medica')
 console.log(funcionario1)
 
@@ -36,12 +65,11 @@ class Paciente {
     paciente: string;
     cpf: string;
     cartaodosus: string;
-    telefone: string;
-    constructor(paciente: string, cpf: string, cartaodosus: string, telefone: string) {
+    constructor(paciente: string, cpf: string, cartaodosus: string) {
+        super(     )
         this.paciente = paciente;
         this.cpf = cpf;
         this.cartaodosus = cartaodosus;
-        this.telefone = telefone;
     }
 }
 const paciente1 = new Paciente('Eduarda', '08882034451', '09999112', '998338875')
